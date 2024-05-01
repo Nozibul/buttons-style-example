eabaee1a4e062b36e9dd50c08e067cdbf385f001
Nozibul <nozibulislamspi@gmail.com>
2024-05-01 10:56:06 +0600

    Added 3 styling button

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Button1 from "./componets/button1/Button1";
+import Button10 from "./componets/button10/Button10";
+import Button2 from "./componets/button2/Button2";
+import Button3 from "./componets/button3/Button3";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Button1 />
+        <Button2 />
+        <Button3 />
+        <Button10 />
+      </div>
+    </>
+  );
+}
+
+export default App;
