3cc7a1421bbf14d10ea73afa13b2e8003a57fb75
Nozibul Islam <nozibulislamspi@gmail.com>
2024-05-10 12:00:43 +0600

    Adding 4 more awesome button on hover effect

--- src/components/button20/Button20.jsx
+++ src/components/button20/Button20.jsx
@@ -1,14 +1,13 @@
-import styles from "./Button20.module.css";
+import Styles from "./Button20.module.css";
 
 const Button20 = () => {
-     return (
-          <div>
-               <button className={`${styles.btn} ${styles.btn1}`}>Hover Me</button>
-               <button className={`${styles.btn} ${styles.btn2}`}>Hover Me</button>
-               <button className={`${styles.btn} ${styles.btn3}`}>Hover Me</button>
-               <button className={`${styles.btn} ${styles.btn4}`}>Hover Me</button>
-          </div>
-     );
+  return (
+       <div>
+            <button className={` ${Styles.purple}`}>Hello!</button>
+            <button className={` ${Styles.lorange}`}>Hello!</button>
+            <button className={` ${Styles.brick}`}>Hello!</button>
+            <button className={` ${Styles.blue}`}>Hello!</button>
+       </div>
+  );
 };
-
 export default Button20;
